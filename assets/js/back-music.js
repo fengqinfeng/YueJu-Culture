6b5ba6a6225a3e333e1a9c069b5ec5c610331989
fqf <937438477@qq.com>
2019-07-08 21:39:41 +0800

    背景音乐

---
+++ assets/js/back-music.js
@@ -0,0 +1,18 @@
+function play() {
+    var audio = document.getElementById('music');
+    if (audio.paused) {
+        $("#musicicon").removeClass("stop");
+        $("#musicicon").addClass("xuanzhuan");
+        audio.play();
+
+        //document.getElementById('musicImg').src="images/play.png";
+    }else{
+        $("#musicicon").removeClass("xuanzhuan");
+        $("#musicicon").addClass("stop");
+
+        audio.pause();
+        audio.currentTime = 0;//音乐从头播放
+
+        //document.getElementById('musicImg').src="images/stop.png";
+    }
+}
