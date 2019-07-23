8f14ffa7502d304005c68088e8d903106eac86b9
fqf <937438477@qq.com>
2019-07-24 05:20:28 +0800

    改版

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,14 @@
-$(document).ready(function(){
-	"use strict";
-    
-        /*==================================
+$(document).ready(function () {
+    "use strict";
+
+    /*==================================
 * Author        : "ThemeSINE"
 * Template Name : Zombiz HTML Template
 * Version       : 1.0
 ==================================== */
 
 
-
-
-        /*=========== TABLE OF CONTENTS ===========
+    /*=========== TABLE OF CONTENTS ===========
 1. Scroll To Top 
 2. hcsticky 
 3. Counter
@@ -20,144 +18,157 @@ $(document).ready(function(){
 ======================================*/
 
     // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
-	
-	
-	// 2 . hcsticky 
-
-		$('#menu').hcSticky();
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 600) {
+            $('.return-to-top').fadeIn();
+        } else {
+            $('.return-to-top').fadeOut();
+        }
+    });
+    $('.return-to-top').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500);
+        return false;
+    });
 
 
-	// 3. counter
-		$(window).on('load', function(){	
-			$('.counter').counterUp({
-				delay: 10,
-				time: 3000
-			});	
-		});
-	
-	
-	// 4. owl carousel
+    // 2 . hcsticky
 
-		// i. .famous-carousel
-	
-		
-		var owl=$('.famous-carousel');
-		owl.owlCarousel({
-			items:4,
-			margin:0,
-			
-			loop:true,
-			autoplay:true,
-			smartSpeed:500,
-			
-			//nav:false,
-			//navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-			
-			dots:false,
-			autoplayHoverPause:true,
-		
-			responsiveClass:true,
-				responsive:{
-					0:{
-						items:1
-					},
-					640:{
-						items:2
-					},
-					768:{
-						items:3
-					},
-					992:{
-						items:4
-					}
-				}
-			
-			
-		});
+    $('#menu').hcSticky();
 
-		// ii. .client (carousel)
-		
-		$('#client').owlCarousel({
-			items:5,
-			loop:true,
-			smartSpeed: 1000,
-			autoplay:true,
-			dots:false,
-			autoplayHoverPause:true,
-			responsive:{
-					0:{
-						items:2
-					},
-					415:{
-						items:2
-					},
-					600:{
-						items:3
-					},
-					1000:{
-						items:5
-					}
-				}
-			});
-			
-			
-			$('.play').on('click',function(){
-				owl.trigger('play.owl.autoplay',[1000])
-			})
-			$('.stop').on('click',function(){
-				owl.trigger('stop.owl.autoplay')
-			})
 
-		// iii.  testimonial
-		
-		$("#testemonial-carousel").owlCarousel({
-			items: 1,
-			autoplay: true,
-			loop: true,
-			dots:true,
-			mouseDrag:true,
-			nav:false,
-			smartSpeed:1000,
-			transitionStyle:"fade",
-			animateIn: 'fadeIn',
-			animateOut: 'fadeOutLeft'
-			// navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
-		});
-
-	// 5. vedio player
-		$('.vedio-play-icon').magnificPopup({
-			
-			type:'video'
-		
-		});	
-
-	// 6. animation support
-
-        $(window).load(function(){
-
-            $(".single-slide-item-content h2, .single-slide-item-content p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".single-slide-item-content button").removeClass("animated fadeInLeft").css({'opacity':'0'});
+    // 3. counter
+    $(window).on('load', function () {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 3000
         });
+    });
 
-        $(window).load(function(){
 
-            $(".single-slide-item-content h2, .single-slide-item-content p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".single-slide-item-content button").addClass("animated fadeInLeft").css({'opacity':'0'});
+    // 4. owl carousel
 
-        });
-		
+    // i. .famous-carousel
+
+
+    var owl = $('.famous-carousel');
+    owl.owlCarousel({
+        items: 4,
+        margin: 0,
+
+        loop: true,
+        autoplay: true,
+        smartSpeed: 500,
+
+        //nav:false,
+        //navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+
+        dots: false,
+        autoplayHoverPause: true,
+
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+
+
+    });
+
+    // ii. .client (carousel)
+
+    $('#client').owlCarousel({
+        items: 5,
+        loop: true,
+        smartSpeed: 1000,
+        autoplay: true,
+        dots: false,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            415: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
+
+    // iii.  testimonial
+
+    $("#testemonial-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        loop: true,
+        dots: true,
+        mouseDrag: true,
+        nav: false,
+        smartSpeed: 1000,
+        transitionStyle: "fade",
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOutLeft'
+        // navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+    });
+    $("#testemonial-carousel2").owlCarousel({
+        items: 1,
+        autoplay: true,
+        loop: true,
+        dots: true,
+        mouseDrag: true,
+        nav: false,
+        smartSpeed: 1000,
+        transitionStyle: "fade",
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOutLeft'
+        // navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+    });
+
+    // 5. vedio player
+    // $('.vedio-play-icon').magnificPopup({
+    //
+    //     type: 'video'
+    //
+    // });
+
+    // 6. animation support
+
+    $(window).load(function () {
+
+        $(".single-slide-item-content h2, .single-slide-item-content p").removeClass("animated fadeInUp").css({'opacity': '0'});
+        $(".single-slide-item-content button").removeClass("animated fadeInLeft").css({'opacity': '0'});
+    });
+
+    $(window).load(function () {
+
+        $(".single-slide-item-content h2, .single-slide-item-content p").addClass("animated fadeInUp").css({'opacity': '0'});
+        $(".single-slide-item-content button").addClass("animated fadeInLeft").css({'opacity': '0'});
+
+    });
+
 });	
 	
